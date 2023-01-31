8b875f925005dceacb9d192c207ecbc3541debce
Ritvik <ritvik@gmail.com>
2023-01-31 10:27:17 -0800

    shows total recipe list --> work on implementing into json data

--- Recipe.java
+++ Recipe.java
@@ -6,7 +6,7 @@ import java.net.http.*;
 public class Recipe {
     public static void main(String[] args ) throws Exception{
         HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup"))
+        .uri(URI.create("https://tasty.p.rapidapi.com/recipes/list"))
         .header("X-RapidAPI-Key", "bc9e5f20f9mshfceb3f679afd2b7p1960cdjsn87cb096651ab")
         .header("X-RapidAPI-Host", "tasty.p.rapidapi.com")
         .method("GET", HttpRequest.BodyPublishers.noBody())
